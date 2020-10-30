b4777c9a5d97502c30c52ed9e475c2f44389d25e
Shyla Earl <shyla.earl@gmail.com>
2020-10-30 10:17:20 -0500

    working on functionality

--- src/components/CustInfoForm/CustInfoForm.js
+++ src/components/CustInfoForm/CustInfoForm.js
@@ -1,33 +1,41 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import './App.css';
-//do I need the long react router, route, etc. import here?
 
 class CustInfoForm extends Component {
 
-    //should I add state here with an object that takes in name, address, etc.?
-    // state = {
-    //     inputValue: ''
-    // }
+    state = {
+        custInfo: {
+            name: event.target.value?,
+            streetAddress:
+            city:
+            zip: 
+            type: Boolean?
+        }
+    }
 
     addCustInfo = () => {
         console.log("customer info form")
         //should capture an object and including info from 1st order page and route to checkout page/component
-        //this.props.dispatch({type: 'ADD_CUST_INFO', payload: this.state.inputValue})
+        this.props.dispatch({type: 'ADD_CUST_INFO', payload: this.state.custInfo})
+        //orderReducer customerInfoReducer
     }
 
     submitCustInfo = () => {
         console.log("clicked",)
         //should route to order checkout page/component
-        //this.props.dispatch({type: 'SUBMIT_CUST_INFO'})
+        this.props.dispatch({type: 'SUBMIT_CUST_INFO'})
+        //POST request here
     }
 
-    // handleChange = (event) => {
-    //     this.setState({
-    //       inputValue: event.target.value
-    //     });
-    //   }
-    //Do I need this here? or is this handled by the addCustInfo function?
+    handleChange = (event) => {
+        this.setState({
+            
+          inputValue: event.target.value
+        });
+    }
+      //do this for all input values to bundle the info as 1 object
+    //Do I need this here? or is this handled by the addCustInfo function? Use for updating total in header and call in submit function
 
     render(){
         return (
