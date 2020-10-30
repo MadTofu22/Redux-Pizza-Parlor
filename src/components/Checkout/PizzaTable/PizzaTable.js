bfc33fa7b5f5972ef1b00acd7f7cdd4c939a422e
Elliot Malcolm <talcumalc@gmail.com>
2020-10-30 10:21:50 -0500

    componentizing

---
+++ src/components/Checkout/PizzaTable/PizzaTable.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Checkout extends Component {
+
+    render() {
+       
+        });
+        return (
+            <div>
+                <h2>'Step 3: Checkout'</h2>
+            </div>
+        )
+    }
+}
