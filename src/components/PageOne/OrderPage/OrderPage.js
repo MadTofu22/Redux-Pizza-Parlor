419b629c5da558435cb9794574b0117f5f9f49a7
MadTofu22 <stutler.tom@gmail.com>
2020-10-30 11:50:37 -0500

    added a lot of stuff

--- src/components/PageOne/OrderPage/OrderPage.js
+++ src/components/PageOne/OrderPage/OrderPage.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Header from '../Header/Header';
+import PizzaList from '../PizzaList/PizzaList';
 
 class OrderPage extends Component {
 
     render () {
         return (
             <div>
+                <Header />
+                <PizzaList />
                 Test
             </div>
         );
--- src/components/PageOne/PizzaItem/PizzaItem.js
+++ src/components/PageOne/PizzaItem/PizzaItem.js
@@ -3,10 +3,38 @@ import {connect} from 'react-redux';
 
 class PizzaItem extends Component {
 
+    state = {
+        clicked: false
+    }
+
+    handleSubmit = () => {
+
+        this.props.dispatch({
+            type: 'ADD_TO_CART', 
+            payload: {
+                name: this.props.pizza.name,
+                price: this.props.pizza.price
+            }
+        })l
+    }
     render () {
-        
+        return (
+            <div>
+                <img src={this.props.pizza.image_path}></img>
+                <div>
+                    <h5>{this.props.pizza.name}</h5>
+                    <p>{this.props.pizza.description}</p>
+                </div>
+                <button id='inCart' onSubmit={this.handleSubmit}>
+                    {this.state.clicked ?
+                    <p>Remove</p>
+                    :
+                    <p>Add</p>}
+                </button>
+            </div>
+        )
     }
 }
 
 const putReduxStateOnProps = (ReduxState) => ({ReduxState});
-export default connect(putReduxStateOnProps)(PizzatItem);
+export default connect(putReduxStateOnProps)(PizzaItem);
